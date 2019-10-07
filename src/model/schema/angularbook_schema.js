7613b964ff60f6f4bce2598bbd8267547f65cdda
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2019-10-06 23:16:22 -0300

    update posts

--- src/model/schema/angularbook_schema.js
+++ src/model/schema/angularbook_schema.js
@@ -16,7 +16,7 @@ var AngularbookSchema = new mongoose.Schema({
     },
     photo: String,
     data: Date.toString,
-    like: [{ user:String, likes:Boolean}],
+    like: [{}],
     comment: String
 
 });
