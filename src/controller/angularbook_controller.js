991f88ca68acddd85d0b92436256ccd410ebfb31
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2019-10-13 20:36:26 -0300

    jwt

--- src/controller/angularbook_controller.js
+++ src/controller/angularbook_controller.js
@@ -138,7 +138,6 @@ router.post("/update/user",verifyToken, (req, res, next) => {
 
 function verifyToken(req, res, next){
     let bearerHeader = req.headers["authorization"];
-    console.log(req.headers);
     console.log(bearerHeader);
     if(typeof bearerHeader !== 'undefined'){
         let bearer = bearerHeader.split(" ")[1];
