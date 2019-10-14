823766f157c0a80e59a689e852e918f82c900340
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2019-10-14 00:21:05 -0300

    user photo

--- src/controller/user.js
+++ src/controller/user.js
@@ -48,7 +48,8 @@ exports.login = function (req, res, next) {
                                 username: doc.username,
                                 name: doc.name,
                                 lastname: doc.lastname,
-                                email: doc.email
+                                email: doc.email,
+                                photo: doc.photo
                               },
                               "c": token,
                               "notification": {
