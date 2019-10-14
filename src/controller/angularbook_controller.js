cb28af5aafb51004d4c610a78462d9e230375386
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2019-10-14 00:32:10 -0300

    new profile

--- src/controller/angularbook_controller.js
+++ src/controller/angularbook_controller.js
@@ -42,9 +42,14 @@ router.post("/list/user/post", verifyToken,(req, res, next) => {
 router.post("/profile",verifyToken, (req, res, next) => {
     jwt.verify(req.token, process.env.KEY, (err, authData)=>{
         if(!err){
+
+            let userprofile = user.porifle(req.res,next);
+            let userposts = posts.profile(req, res, next);
+            console.log(userprofile);
+            console.log(userposts); 
             let obj = {
-                "user" : user.porifle(req.res,next),
-                "posts": posts.profile(req, res, next),
+                "user" : userprofile ,
+                "posts": userposts
             };
             res.send(obj);
             
